4baa4b17a6decbaaef7bc36c54950d8cefb00401
Anastasia Kuzmenko <knigoed92@gmail.com>
2020-07-11 23:53:30 +0300

    adds basic tests for Participant

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/participants', 'ParticipantsController@store');
+Route::patch('/participants/{participant}', 'ParticipantsController@update');
---
+++ tests/Feature/ParticipantTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Participant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ParticipantTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function testAddParticipantInDB()
+    {
+        $this->withoutExceptionHandling();
+
+       $response = $this->post('participants', [
+           'first_name' => 'David',
+           'last_name' => 'Linch',
+           'email' => 'davidlinch@gmail.com'
+       ]);
+
+       $response->assertOk();
+       $this->assertCount(1, Participant::all());
+    }
+
+    /** @test */
+    public function testFirstNameIsRequired()
+    {
+        $response = $this->post('participants', [
+            'first_name' => '',
+            'last_name' => 'Laurentius',
+            'email' => 'laurentius@gmail.com'
+        ]);
+
+        $response->assertSessionHasErrors('first_name');
+    }
+
+    /** @test */
+    public function testLastNameIsRequired()
+    {
+        $response = $this->post('participants', [
+            'first_name' => 'Laura',
+            'last_name' => '',
+            'email' => 'laura@gmail.com'
+        ]);
+
+        $response->assertSessionHasErrors('last_name');
+    }
+
+    /** @test */
+    public function testEmailIsRequired()
+    {
+        $response = $this->post('participants', [
+            'first_name' => 'Anna',
+            'last_name' => 'Maria',
+            'email' => ''
+        ]);
+
+        $response->assertSessionHasErrors('email');
+    }
+
+    public function testUpdateParticipant()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('participants', [
+            'first_name' => 'David',
+            'last_name' => 'Linch',
+            'email' => 'davidlinch@gmail.com'
+        ]);
+
+        $participant = Participant::first();
+
+        $response = $this->patch('/participants/' . $participant->id, [
+            'first_name' => 'Max',
+            'last_name' => 'Ginger',
+            'email' => 'maxginger@gmail.com'
+        ]);
+
+        $this->assertEquals('Max', Participant::first()->first_name);
+        $this->assertEquals('Ginger', Participant::first()->last_name);
+        $this->assertEquals('maxginger@gmail.com', Participant::first()->email);
+    }
+}
