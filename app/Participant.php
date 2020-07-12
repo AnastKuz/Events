278d8d8076b2e1452ec551f61ff6f9233f237e90
Anastasia Kuzmenko <knigoed92@gmail.com>
2020-07-12 21:47:38 +0300

    added belongsTo for Participant

--- app/Participant.php
+++ app/Participant.php
@@ -8,4 +8,9 @@ class Participant extends Model
 {
     protected $fillable = ['first_name', 'last_name', 'email'];
     protected $guarded = ['id','created_at','updated_at'];
+
+    public function events()
+    {
+        return $this->belongsTo(Event::class,'product_id','id');
+    }
 }
